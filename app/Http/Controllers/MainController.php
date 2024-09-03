878ad90dbd1acecaa823e5c75e0236832171f105
arsalan-khan17 <arsalankhan4014@gmail.com>
2024-09-04 00:54:18 +0500

    added cache_clear

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Scholarship;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class MainController extends Controller
 {
@@ -17,5 +18,17 @@ class MainController extends Controller
         Scholarship::create($data);
         return redirect('/')->with('message','Added Successfully');
 
+    }
+
+    function clear_cache(){
+        Artisan::call('cache:clear');
+        Artisan::call('route:clear');
+        Artisan::call('config:clear');
+        return redirect('/');
+    }
+
+    function run_migrations(){
+        Artisan::call('migrate');
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[MainController::class,'index'])->name('home');
 
 Route::post('/save_scholarship',[MainController::class,'save_scholarship'])->name('save_scholarship');
+Route::get('/clear_cache',[MainController::class,'clear_cache'])->name('clear_cache');
+Route::get('/run_migrations',[MainController::class,'run_migrations'])->name('run_migrations');
